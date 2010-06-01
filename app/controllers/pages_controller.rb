37e8c2ccca615192ad1ccf68eca54117c03a1ea8
Rik Chilvers <rik@plus.gd>
2010-06-01 22:00:19 +0100

    Made titles of home, about and contact dynamic
    @title instance variable set in pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Stabilisers - Home'
   end
 
   def contact
+    @title = 'Stabilisers - Contact'
   end
   
   def about
+    @title = 'Stabilisers - About'
   end
 
 end
