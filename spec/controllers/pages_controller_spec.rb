806debdc76effd30d341ff1ce8f19edd21d71a4f
Rik Chilvers <rik@plus.gd>
2010-06-01 16:37:35 +0100

    Red testing for about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe PagesController do
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
