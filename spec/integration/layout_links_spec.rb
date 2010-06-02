4d53f10392edf3d188ce121fe1bf748510f0671e
Rik Chilvers <rik@plus.gd>
2010-06-02 16:45:03 +0100

    Created routes, added tests for checking the routes work

---
+++ spec/integration/layout_links_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should render_template('pages/home')
+  end
+  
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should render_template('pages/contact')
+  end
+  
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should render_template('pages/about')
+  end 
+  
+end
